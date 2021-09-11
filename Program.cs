9e4ee0783a467ac1118b162cc543a053e64e7668
Hillarie-kip <kalyahillary@gmail.com>
2021-09-11 22:14:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace c_sharp_demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            reverseWord("Hello World!");
+        }
+
+
+
+
+
+        static void reverseWord(string str)
+        {
+            char[] array = str.ToCharArray();
+
+            Console.WriteLine(array);
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                array[i] = str[j];
+                array[j] = str[i];
+
+            }
+            string reversed = new string(array);
+            Console.WriteLine(reversed);
+
+        }
+    }
+}
