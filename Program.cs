3d1ef0b25a0ea55c2fe39aae2519ad0516672de8
Hillarie <Hillarie@HK>
2021-09-18 23:33:45 +0300

    convert a two-dimensional array to a one-dimensional array

--- Program.cs
+++ Program.cs
@@ -62,5 +62,32 @@ namespace c_sharp_demos
             Console.WriteLine(reversed);
 
         }
+
+
+       // convert a two-dimensional array to a one-dimensional array?
+        internal static void MultiToSingle(int[,] array)
+        {
+            //Convert 2-D array to 1-D array
+
+            /*Input: { { 1, 2, 3 }, { 4, 5, 6 } }, output: 1 4 2 5 3 6 
+             * 
+             * */
+
+            int index = 0;
+            int width = array.GetLength(0);
+            int height = array.GetLength(1);
+            int[] single = new int[width * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    single[index] = array[x, y];
+                    Console.Write(single[index] + " ");
+                    index++;
+                }
+            }
+        }
+
     }
 }
